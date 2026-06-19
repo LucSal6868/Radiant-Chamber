74fd1004a2ddc26a48f6782b7c8407da3fb2e8a6
Lucas Salinas <salinaslucas19@gmail.com>
2026-06-19 18:04:50 -0400

    simple shrink grow button

--- Assets/Scripts/Buttons/ChangeSize.cs
+++ Assets/Scripts/Buttons/ChangeSize.cs
@@ -2,15 +2,23 @@ using UnityEngine;
 
 public class ChangeSize : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    
+
+    private bool player_normal = true;
+    [SerializeField] private GameObject player;
+
+    public void changer_player_size()
     {
-        
+        if (player_normal)
+        {
+            player.transform.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+            player_normal = false;
+        }
+        else
+        {
+            player.transform.localScale = new Vector3(1f, 1f, 1f);
+            player_normal = true;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
