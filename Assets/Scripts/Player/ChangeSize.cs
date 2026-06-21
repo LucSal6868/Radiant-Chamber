3bcdeadda726cb2fcc9e5d744929ac25dc943695
Lucas Salinas <salinaslucas19@gmail.com>
2026-06-20 22:21:12 -0400

    fixed?
    might work with headset

--- Assets/Scripts/Player/ChangeSize.cs
+++ Assets/Scripts/Player/ChangeSize.cs
@@ -1,48 +1,43 @@
-using System.Collections;
 using UnityEngine;
 
-public class ChangeSize : MonoBehaviour
+public class ShrinkPlayer : MonoBehaviour
 {
-    private bool isSmall = false;
-    private bool isChangingSize = false;
+    public float normalSize = 5f;
+    public float shrinkSize = 1f;
+    public float shrinkSpeed = 2f;
 
-    public void ToggleSize()
+    private bool isShrunk = false;
+    private bool isTransitioning = false;
+    private CharacterController controller;
+
+    void Start()
     {
-        if (!isChangingSize)
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        if (isTransitioning)
         {
-            float targetScale = isSmall ? 1f : 0.1f;
-            StartCoroutine(ChangeSizeCoroutine(targetScale));
+            float targetSize = isShrunk ? shrinkSize : normalSize;
+            transform.localScale = Vector3.Lerp(transform.localScale, new Vector3(targetSize, targetSize, targetSize), shrinkSpeed * Time.deltaTime);
+
+            if (Vector3.Distance(transform.localScale, new Vector3(targetSize, targetSize, targetSize)) < 0.01f)
+            {
+                transform.localScale = new Vector3(targetSize, targetSize, targetSize);
+                isTransitioning = false;
+            }
         }
     }
 
-    private IEnumerator ChangeSizeCoroutine(float scale)
+    public void ToggleSize()
     {
-        isChangingSize = true;
+        isShrunk = !isShrunk;
+        isTransitioning = true;
+    }
 
-        Vector3 startScale = transform.localScale;
-        Vector3 targetScale = Vector3.one * scale;
-
-        float duration = 0.5f;
-        float elapsed = 0f;
-
-        while (elapsed < duration)
-        {
-            elapsed += Time.deltaTime;
-
-            float t = elapsed / duration;
-
-            transform.localScale = Vector3.Lerp(
-                startScale,
-                targetScale,
-                t
-            );
-
-            yield return null;
-        }
-
-        transform.localScale = targetScale;
-
-        isSmall = scale < 1f;
-        isChangingSize = false;
+    public bool IsMovementAllowed()
+    {
+        return !isTransitioning;
     }
 }
